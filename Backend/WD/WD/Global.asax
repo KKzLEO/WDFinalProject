f01985dcbcf7d8f15ffefd8fc16a5a5f9a83d808
Leo <avril9950@gmail.com>
2018-06-17 15:29:44 +0800

    Revert "移資料夾"
    This reverts commit 5a1037cd891f38c209591b028ee5af9d956820e7.

---
+++ Backend/WD/WD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WD.MvcApplication" Language="C#" %>
---
+++ Backend/WD/WD/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WD.App_Start;
+
+namespace WD
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // 應用程式啟動時執行的程式碼
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
